e009858d7829cfe48097d5f94a3f2e789d4837fc
Amelia Schulz <you@example.com>
2018-11-01 10:14:22 -0400

    adds dummy data

--- schema.sql
+++ schema.sql
@@ -1,1 +1,20 @@
 -- should only contain the create table statements
+create table players (
+    id serial primary key,
+    name varchar(100),
+    
+);
+
+create table games (
+    id serial primary key,
+    
+    player_1 score integer,
+    player_2 score integer,
+
+    winner_id integer references players (id),
+
+    -- foreign keys at the bottom
+    player1_id integer references players (id),
+    player2_id integer references players (id)
+    
+);
--- seed.sql
+++ seed.sql
@@ -1,1 +1,22 @@
 -- fill the tables with dummy data
+
+insert into players
+    (name)
+values
+    ('evan'),
+    ('ignacio'),
+    ('ed'),
+    ('not-evan'),
+    ('lorenzo'),
+    ('sam'),
+    ('anokhee');
+
+insert into games
+    (player1_score, player2_score, player1_id, player_2id)
+values  
+    (21, 0, 2, 3),
+    (21, 2, 2, 3),
+    (21, 1, 2, 3),
+    (13, 21, 3, 1),
+    (21, 2, 5, 1),
+    (10, 21, 4, 2);
